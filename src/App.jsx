11291877c76a20ad3a569a9c49d8694c825c7bed
sungseanpark <sungseanpark@gmail.com>
2024-03-30 15:27:51 -0700

    Complete part 7.4

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {
   useNavigate
 } from 'react-router-dom'
 import { useState } from 'react'
+import  { useField } from './hooks'
 
 // const Menu = () => {
 //   const padding = {
@@ -69,18 +70,18 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const navigate = useNavigate()
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
@@ -92,15 +93,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
@@ -138,7 +139,7 @@ const App = () => {
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000)
     setAnecdotes(anecdotes.concat(anecdote))
-    setNotification(`a new anecdote '${anecdote.content}'`)
+    setNotification(`a new anecdote '${anecdote.content}' has been added`)
     setTimeout(() => {
       setNotification('')
     }, 5000)
